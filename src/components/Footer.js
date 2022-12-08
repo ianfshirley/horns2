b25fd4b90a340d44ecce72bab3d8abe5936ac369
ianfshirley <ianfshirley88@gmail.com>
2022-12-07 22:35:19 -0800

    app setup complete, need to build horned beast component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <footer>© 2022 Ian F. Shirley</footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/HornedBeast.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+
+  render() {
+    return (
+      <p>horned beast</p>
+    );
+  }
+}
+
+export default HornedBeast;
